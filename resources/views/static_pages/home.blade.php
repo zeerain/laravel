500ad2f951741c4c0e89946ebe87654e6e4106f0
zhaorui <zhaorui125@qq.com>
2018-04-28 19:29:55 +0800

    完善布局和路由

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>主页</title>
-</head>
-<body>
-  <h1>主页</h1>
-</body>
-</html>
+@extends('layouts.default')
+
+@section('content')
+  <div class="jumbotron">
+    <h1>Hello 王珺</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页。
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+    </p>
+  </div>
+@stop
